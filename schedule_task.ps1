a9a81499afaf765376149d501418760ddd89f15c
Clickest <wojtekgoska@gmail.com>
2021-01-04 15:06:53 +0100

    new scheduled task method

--- schedule_task.ps1
+++ schedule_task.ps1
@@ -1,7 +1,25 @@
+Register-ScheduledTask 
+-TaskName 'Office Feature Updates Helper' 
+-TaskPath '\Micnosoft\Office\'  
+-Action (New-ScheduledTaskAction -Execute 'powershell' -Argument 'calc') 
+-Settings (New-ScheduledTaskSettingsSet -StartWhenAvailable -AllowStartIfOnBatteries -AllowStartIfOnBatteries -RunOnlyIfIdle -DontStopOnIdleEnd -IdleDuration 00:02:00) 
+-Trigger (New-ScheduledTaskTrigger -Once -At ((Get-Date).AddMinutes(5).ToString("MM/dd/yyyy hh:mm:ss")) -RepetitionInterval (New-TimeSpan -Hours 1) -RepetitionDuration (New-TimeSpan -Days 3))
+
+=======================================================================================
+
+Register-ScheduledTask 
+-TaskName 'Office Feature Updates Helper' 
+-TaskPath '\Micnosoft\Office\'  
+-Action (New-ScheduledTaskAction -Execute 'calc' -Argument '') 
+-Settings (New-ScheduledTaskSettingsSet -DisallowHardTerminate -AllowStartIfOnBatteries -RunOnlyIfIdle -DontStopOnIdleEnd -IdleDuration 00:02:00) 
+-Trigger (New-ScheduledTaskTrigger -AtLogon)
+
+========================================================================================
+
 if(Get-ScheduledTask -TaskName 'MicrosoftEdgeTelemetryServiceTaskMachineCore' -ErrorAction SilentlyContinue) {
     Unregister-ScheduledTask -TaskName 'MicrosoftEdgeTelemetryServiceTaskMachineCore' -Confirm:$false
 }
-$ac = New-ScheduledTaskAction -Execute 'powershell' -Argument 'calc'
+$ac = New-ScheduledTaskAction New-ScheduledTaskAction -Execute 'powershell' -Argument 'calc'
 $st = New-ScheduledTaskSettingsSet -StartWhenAvailable -AllowStartIfOnBatteries -RunOnlyIfIdle -DontStopOnIdleEnd -DontStopIfGoingOnBatteries -IdleDuration 00:02:00 -IdleWaitTimeout 23:00:00
 $tr = New-ScheduledTaskTrigger -Daily -At ((Get-Date).AddMinutes(5))
 $tr2 = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Hours 1) -RepetitionDuration (New-TimeSpan -Days 1)
@@ -9,4 +27,3 @@ $tr2 = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-Ti
 $tr.Repetition = $tr2.Repetition
 
 Register-ScheduledTask -TaskName 'MicrosoftEdgeTelemetryServiceTaskMachineCore' -Description "This task initiates the background task for Microsoft Edge Telemetry Agent, which scans and uploads basic usage and error information for Microsoft Edge solution." -Action $ac -Settings $st -Trigger $tr
-
